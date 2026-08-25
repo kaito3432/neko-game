b91b21245229bfcf62b5317653280276af51f402
kaito3432 <147011231+kaito3432@users.noreply.github.com>
2026-08-25 13:37:00 +0900

    Update game.js

--- game.js
+++ game.js
@@ -129,14 +129,7 @@ const fakePawBannerText=$("fakePawBannerText");
   const privacyOverlay=$("privacyOverlay"),privacyIcon=$("privacyIcon");
   const privacyTitle=$("privacyTitle"),privacyText=$("privacyText"),privacyBtn=$("privacyBtn");
   const victoryCutin=$("victoryCutin"),victoryCutinImage=$("victoryCutinImage");
-   // =====================================
-// 特殊スキル 発動カットイン
-// =====================================
-const skillCutin = $("skillCutin");
-const skillCutinKicker = $("skillCutinKicker");
-const skillCutinImage = $("skillCutinImage");
-const skillCutinName = $("skillCutinName");
-const skillCutinDesc = $("skillCutinDesc");
+
 
 let skillCutinTimer = null;
   const resultOverlay=$("resultOverlay"),resultIcon=$("resultIcon");
